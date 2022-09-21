4d3fc43e2e172df8fb427cf01f1b4b3f6980f6ae
phillythegremlin <obioratheophilus@gmail.com>
2022-09-20 23:25:51 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -10,4 +10,3 @@ int main(void)
 	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
